4b93fb63daa5e2b8906cdc1f200eb77a6652f107
Jakub Zielinski <jakubzielinski@hopsoftware.com>
2021-07-10 19:11:00 +0100

    Update

--- TT_Education_webAPI.API/Program.cs
+++ TT_Education_webAPI.API/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace TT_Education_webAPI.API
 {
@@ -32,6 +33,11 @@ namespace TT_Education_webAPI.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
